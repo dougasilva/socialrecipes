00bd9757eaec2e81d0ab5818a0dc0f43fb727ce6
Douglas <douglasasilva@gmail.com>
2015-11-12 16:27:22 -0200

    Correção

--- spec/features/define_root_path_and_list_last_20_recents_recipes_spec.rb
+++ spec/features/define_root_path_and_list_last_20_recents_recipes_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 feature 'Define root path and list recipes' do
   scenario 'Root path is recipes list successffully' do
     visit root_path
-    expect(page).to have_content('RECEITAS')
+    expect(page).to have_content 'Cozinhas'
   end
 end
